f1b660681d038757c784c503a11f2835715e822b
ricaiueo <angelinarica1@gmail.com>
2026-01-31 16:33:38 +0800

    chatbot

---
+++ app.py
@@ -0,0 +1,40 @@
+import google.generativeai as genai
+import streamlit as st
+
+st.title("The Musketeers Bot")
+
+genai.configure(api_key=st.secrets["API_KEY"])
+model = genai.GenerativeModel('gemini-2.5-flash')
+
+#Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+#Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+#React to user input
+if prompt := st.chat_input("Ask anything"):
+    #Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    #Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        history = [
+            {"role": "user" if m["role"] == "user" else "model", "parts": [m["content"]]}
+            for m in st.session_state.messages[:-1]
+        ]
+        chat = model.start_chat(history=history)
+        
+        # Use stream=True for a better UX
+        response_stream = chat.send_message(prompt, stream=True)
+        
+        # Stream the response to the UI
+        full_response = st.write_stream(res.text for res in response_stream)
+
+    # Add assistant response to history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
